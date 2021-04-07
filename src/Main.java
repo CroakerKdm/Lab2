4e7fbe151c0b42af9632e4147e65957c1bfa18c3
Croaker <CroakerKdm@gmail.com>
2021-04-07 18:20:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author Croaker
+ * @version 1.0.0
+ * @project Lab2
+ * @class Main
+ * @since 07.04.2021 - 18.19
+ **/
+public class Main {
+    public static void main(String[] args) {
+        //code will be here, by the end of tomorrow, sorry for the delay. 
+    }
+}
